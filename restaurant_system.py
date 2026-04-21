fcba9d6cd81b39b6282d5f489221c4b25407821b
ArnasMark <arnasmark9@gmail.com>
2026-04-21 16:36:20 +0300

    Add methods for final price and item type in FoodItem

--- restaurant_system.py
+++ restaurant_system.py
@@ -25,3 +25,9 @@ class FoodItem(MenuItem):
     def __init__(self, item_id: int, name: str, price: float, vegan: bool = False):
         super().__init__(item_id, name, price)
         self.vegan = vegan
+
+    def final_price(self) -> float:
+        return self.price
+
+    def item_type(self) -> str:
+        return "Food"
