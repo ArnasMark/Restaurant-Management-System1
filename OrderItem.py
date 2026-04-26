6646d8c748e5c053ebfdb72bd3b181a29cbcaca7
ArnasMark <arnasmark9@gmail.com>
2026-04-26 09:30:21 +0300

    Delete OrderItem.py

--- OrderItem.py
+++
@@ -1,9 +0,0 @@
-class OrderItem:
-    def __init__(self, menu_item: MenuItem, quantity: int):
-        if quantity <= 0:
-            raise ValueError("Quantity must be greater than zero.")
-        self.menu_item = menu_item
-        self.quantity = quantity
-
-    def subtotal(self) -> float:
-        return self.menu_item.final_price() * self.quantity
